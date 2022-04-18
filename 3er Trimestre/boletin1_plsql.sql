4b3c259596c52e17c10959ac773530f7935c181d
roman millan <romandos962m@gmail.com>
2022-04-18 18:39:23 +0200

    terminado boletin1_plsql

--- 3er Trimestre/boletin1_plsql.sql
+++ 3er Trimestre/boletin1_plsql.sql
@@ -118,7 +118,7 @@ BEGIN
 END;
 
 BEGIN
-	igualOdistinto(7,2,9);
+	igualOdistinto(7,3,9);
 END;
 
 
@@ -148,9 +148,171 @@ END;
  * si el número es par, e impar en caso contrario.
  * */
 
+CREATE OR REPLACE 
+PROCEDURE parOimpar(numero NUMBER)
+IS
+BEGIN
+	IF (MOD(numero,2)=0) THEN
+		DBMS_OUTPUT.PUT_LINE('Par');
+	ELSE
+		DBMS_OUTPUT.PUT_LINE('Impar');
+	END IF;
+END;
+
+BEGIN
+	parOimpar(4);
+END;
 
 
+/*9.
+Realizar un procedure que reciba un número y diga si el número es 
+“Par positivo”, “Par negativo”, “Impar positivo”, “Impar negativo” o “cero”,
+ según sea el número.
+*/
+CREATE OR REPLACE 
+PROCEDURE parOimparPosNega(numero NUMBER)
+IS
+BEGIN
+    IF(numero = 0) THEN
+        DBMS_OUTPUT.PUT_LINE('Cero');
+	ELSIF (MOD(numero,2)=0) THEN
+        IF(numero> 0) THEN
+            DBMS_OUTPUT.PUT_LINE('Par Positivo');
+		ELSE
+            DBMS_OUTPUT.PUT_LINE('Par Negativo');
+		END IF;
+	ELSE 
+        IF (numero > 0) THEN
+            DBMS_OUTPUT.PUT_LINE('Impar Positivo');
+		ELSE
+            DBMS_OUTPUT.PUT_LINE('Impar Negativo');
+		END IF;
+	END IF;
+END;
 
+BEGIN
+	parOimparPosNega(-4);
+END;
 
+/*10.
+Realizar un procedure que reciba dos números como parámetro, y muestre la suma 
+de los dos números.
+*/
 
+CREATE OR REPLACE 
+PROCEDURE suma(num1 NUMBER, num2 NUMBER)
+IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('La suma es: ' || (num1 + num2));
+END;
 
+BEGIN
+    suma(5,6);
+END;
+
+/*11.
+Realizar un procedure que reciba dos números como parámetros y muestre
+ la resta de los dos números.
+*/
+
+CREATE OR REPLACE 
+PROCEDURE resta(num1 NUMBER, num2 NUMBER)
+IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('La resta es: ' || (num1 - num2));
+END;
+
+BEGIN
+    resta(5,6);
+END;
+
+/*12.
+Realizar un procedure que reciba dos número como parámetros y muestre la 
+suma de los dos números si los dos números son mayor que cero, y la resta 
+de los dos números si alguno de los números es menor que cero.
+*/
+
+CREATE OR REPLACE 
+PROCEDURE restaOsuma(num1 NUMBER, num2 NUMBER)
+IS
+BEGIN
+    IF(num1 > 0 AND num2 > 0) THEN
+        DBMS_OUTPUT.PUT_LINE('La suma es: ' || (num1 + num2));
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('La resta es: ' || (num1 - num2));
+    END IF;
+END;
+
+BEGIN
+    restaOsuma(5,-2);
+END;
+
+-- Otra forma y funciona bien
+CREATE OR REPLACE 
+PROCEDURE otraForma(num1 NUMBER, num2 NUMBER)
+IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('El resultado: ' || (num1 + num2));
+END;
+
+BEGIN
+    otraForma(5,-2);
+END;
+
+/*13.
+Realizar un procedure que reciba dos número como parámetros y muestre la suma 
+de los dos números si los dos números son mayor que cero o si los dos números 
+son menor que cero. Si un número es positivo y el otro negativo, muestre la 
+resta del número positivo menos el número negativo.
+*/
+
+CREATE OR REPLACE 
+PROCEDURE restaOsumaRara(num1 NUMBER, num2 NUMBER)
+IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('El resultado: ' || (num1 + num2));
+END;
+
+BEGIN
+    restaOsumaRara(5,-2);
+END;
+
+/*
+    14. Realizar un procedure que calcule el salario neto semanal de un trabajador. 
+    El procedure recibirá como parámetro el numero de horas trabajadas y el precio
+     por hora, y hay que tener en cuenta los siguientes aspectos:
+    • Las primeras 35 horas se pagan a tarifa normal
+    • Las horas que pasen de 35 se pagan a 1,5 veces la tarifa normal.
+    • Las tasa de impuesto son:
+    • Las primeros 600 euros son libres de impuestos.
+    • Los siguientes 300 euros tienen un 25% de impuestos.
+    • Los restantes euros un 45 % de impuestos. 
+*/
+
+CREATE OR REPLACE 
+PROCEDURE calcularSalario(horas NUMBER, precio NUMBER)
+AS
+salario NUMBER(10,2);
+salarioAquitar NUMBER(10,2);
+coste NUMBER(10,2);
+BEGIN
+    IF(horas <= 35)THEN
+        salario := horas*precio;   
+    ELSE
+        salario := (horas * precio)*1.5;
+    END IF;
+    IF(salario > 600 AND salario <= 900)THEN
+        salarioAquitar := salario - 600;
+        coste := salarioAquitar * 0.25;
+        salario := salario - coste;
+    ELSIF(salario > 900)THEN   
+        salarioAquitar:= salario - 900;
+        coste := (salarioAquitar * 0.45) + 75;
+        salario := salario - coste;  
+    END IF;
+    DBMS_OUTPUT.PUT_LINE('Salario Neto: ' || salario);
+END;
+
+BEGIN
+    calcularSalario(80,8);
+END;
